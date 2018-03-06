55c53a6d466d8764d66dd3a122903e234e098f11
DrAlbertCruz <acruz37@csub.edu>
2018-03-05 22:25:58 -0800

    First nightly

---
+++ fun_preprocessImage.m
@@ -0,0 +1,13 @@
+function Iout = fun_preprocessImage(filename)
+% Load the image
+I = imread(filename);
+% Some images may be grayscale. Replicate the image 3 times to
+% create an RGB image.
+if ismatrix(I)
+    I = cat(3,I,I,I);
+end
+% Resize the image as required for the CNN.
+Iout = imresize(I, [227 227]);
+% Typecast into single [0,1]
+Iout = single(mat2gray(Iout));
+end
--- pd_detector.m
+++ pd_detector.m
@@ -58,6 +58,7 @@ handles.output = hObject;
 % Update handles structure
 guidata(hObject, handles);
 
+%% Initialization
 % Initialize panes
 im_blank = [1];
 im_blank_cv = ones(10,100);
@@ -67,6 +68,23 @@ imshow( im_blank_cv, [], 'parent', handles.cv_pd );
 imshow( im_blank_cv, [], 'parent', handles.cv_blackrot );
 imshow( im_blank_cv, [], 'parent', handles.cv_esca );
 imshow( im_blank_cv, [], 'parent', handles.cv_leafspot );
+% Set previous path to this directory
+userData.prevPath = cd;
+set( handles.menu_analyzeImage, 'UserData', userData );
+% Load the neural network, store it in menu_file
+net = load( 'alexNet_Grapevine_PD.mat' );
+set( handles.menu_file, 'UserData', net );
+% Initialize the CSV
+data.data = {[]};
+data.data(1,1) = {'Filename'};
+data.data(1,2) = {'Black rot confidence'};
+data.data(1,3) = {'Control confidence'};
+data.data(1,4) = {'Esca confidence'};
+data.data(1,5) = {'Leaf spot confidence'};
+data.data(1,6) = {'PD confidence'};
+data.dataCounter = 2;
+set( handles.menu_quit, 'UserData', data );
+
 
 
 % --- Outputs from this function are returned to the command line.
@@ -89,10 +107,110 @@ function menu_file_Callback(hObject, eventdata, handles)
 
 % --------------------------------------------------------------------
 function menu_analyzeImage_Callback(hObject, eventdata, handles)
-[FileName,PathName] = uigetfile( ...
-    { '*.jpg;*.jpeg;*.JPG;*.JPEG', 'Joint Photographic Experts Group (*.jpg, *.jpeg, *.JPG, *.JPEG' } );
+% This callback's 'UserData' contains the previous path.
+thisUserData = get( hObject, 'UserData' );
+% User data is organized as follows
+%   thisUserData.prevPath - The previous path
+%   thisUserData.image - The image
+
+% Get experimental results from menu_quit
+thisExperimentalResults = get( handles.menu_quit, 'UserData' );
+
+[file,path] = uigetfile( fullfile( thisUserData.prevPath, '*.*' ), 'Load an image...' );
+
+if path ~= 0
+    thisUserData.prevPath = path; % Save previous path
+    
+    try
+        FILE_NAME = fullfile( path, file );
+        image = fun_preprocessImage( FILE_NAME );
+    catch e
+        error( 'Error when loading the image in menu_analyzeImage callback.' );
+    end
+    thisUserData.image = image; % Save image
+    
+    % Display the image in the preview pane
+    try
+        imshow( image, [], 'Parent', handles.pane_preview );
+    catch e
+        error( 'Error when attempting to display preview of image.' );
+    end
+    
+    % Get the network from menu_file
+    try
+        network = get( handles.menu_file, 'UserData' );
+        network = network.results.net;
+    catch e
+        error( 'Error when attempting to get the network data from handles.menu_file' );
+    end
+    
+    % Do classification
+    try
+        [~,scores] = classify(network,image);
+    catch e
+        error( 'Error when doing classification' );
+    end
+    
+    % Set the confidence values
+    % Order is as follows:
+    %   1 - black rot
+    %   2 - control
+    %   3 - esca
+    %   4 - leaf spot
+    %   5 - pd
+    try
+        % Set the filename
+        thisExperimentalResults.data(thisExperimentalResults.dataCounter,1) = ...
+            {file};
+        for i=1:5
+            confValues = zeros(10,100);
+            thisScore = scores(i);
+            thisScoreText = num2str(thisScore,'%.4f');
+            confValues(:,...
+                1:round(thisScore*100)+1 ) = 1;
+            switch i
+                case 1
+                    imshow( confValues, [], 'parent', handles.cv_blackrot );
+                    set( handles.val_blackrot, 'String', thisScoreText );
+                    thisExperimentalResults.data(thisExperimentalResults.dataCounter,2) = ...
+                        {thisScore};
+                case 2
+                    imshow( confValues, [], 'parent', handles.cv_control );
+                    set( handles.val_control, 'String', thisScoreText );
+                    thisExperimentalResults.data(thisExperimentalResults.dataCounter,3) = ...
+                        {thisScore};
+                case 3
+                    imshow( confValues, [], 'parent', handles.cv_esca );
+                    set( handles.val_esca, 'String', thisScoreText );
+                    thisExperimentalResults.data(thisExperimentalResults.dataCounter,4) = ...
+                        {thisScore};
+                case 4
+                    imshow( confValues, [], 'parent', handles.cv_leafspot );
+                    set( handles.val_leafspot, 'String', thisScoreText );
+                    thisExperimentalResults.data(thisExperimentalResults.dataCounter,5) = ...
+                        {thisScore};
+                otherwise
+                    imshow( confValues, [], 'parent', handles.cv_pd );
+                    set( handles.val_pd, 'String', thisScoreText );
+                    thisExperimentalResults.data(thisExperimentalResults.dataCounter,6) = ...
+                        {thisScore};
+            end
+        end
+    catch e
+        error( 'Error when setting confidence images' );
+    end
+end
+
+% Save user data
+thisExperimentalResults.dataCounter = thisExperimentalResults.dataCounter + 1;
+set( hObject, 'UserData', thisUserData );
+set( handles.menu_quit, 'UserData', thisExperimentalResults );
 
 
 % --------------------------------------------------------------------
 function menu_quit_Callback(hObject, eventdata, handles)
+thisResults = get( hObject, 'UserData' );
+x = inputdlg('Enter file name to save experimental results to:',...
+             'Filename', [1 50]);
+xlswrite( [ cell2mat(x), '.xls' ], thisResults.data );
 close all force
